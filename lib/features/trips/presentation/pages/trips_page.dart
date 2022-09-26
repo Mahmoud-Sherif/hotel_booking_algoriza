aa62c1e8311d75a127ff26a3da613fd8324ef82d
hassanabdellatif <hassanahmmoh@gmail.com>
2022-09-26 20:11:52 +0200

    trips page

--- lib/features/trips/presentation/pages/trips_page.dart
+++ lib/features/trips/presentation/pages/trips_page.dart
@@ -1,79 +1,89 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hotel_booking_algoriza/core/api/api_consumer.dart';
+import 'package:hotel_booking_algoriza/features/trips/presentation/cubit/trips_cubit.dart';
 import 'package:hotel_booking_algoriza/features/trips/presentation/widgets/favorites.dart';
 import 'package:hotel_booking_algoriza/features/trips/presentation/widgets/finished.dart';
 import 'package:hotel_booking_algoriza/features/trips/presentation/widgets/upcoming.dart';
+import 'package:hotel_booking_algoriza/injection_container.dart';
 
 class TripsPage extends StatelessWidget {
   const TripsPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey.shade50,
-      appBar: AppBar(
-        title: const Text(
-          'My Trip',
-          style: TextStyle(
-            color: Colors.black,
-            fontSize: 24,
-            fontWeight: FontWeight.bold,
+    return BlocProvider(
+      create: (context) => TripsCubit(apiConsumer: sl<ApiConsumer>())
+        ..getAllHotelsByTypeUpcomming()
+        ..getAllHotelsByTypeCompleted()
+        ..getAllHotelsByTypeCancelled(),
+      child: Scaffold(
+        backgroundColor: Colors.grey.shade50,
+        appBar: AppBar(
+          title: const Text(
+            'My Trip',
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
           ),
+          backgroundColor: Colors.transparent,
+          elevation: 0,
         ),
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-      ),
-      body: DefaultTabController(
-        length: 3,
-        initialIndex: 0,
-        child: Center(
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16),
-                child: Container(
-                  width: double.infinity,
-                  height: 50,
-                  decoration: BoxDecoration(
-                    borderRadius: const BorderRadius.all(
-                      Radius.circular(20),
-                    ),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.grey.withOpacity(0.1),
-                        spreadRadius: 1,
-                        blurRadius: 4,
-                        offset: const Offset(0, 4),
+        body: DefaultTabController(
+          length: 3,
+          initialIndex: 0,
+          child: Center(
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16),
+                  child: Container(
+                    width: double.infinity,
+                    height: 50,
+                    decoration: BoxDecoration(
+                      borderRadius: const BorderRadius.all(
+                        Radius.circular(20),
                       ),
-                    ],
-                    color: Colors.white,
-                  ),
-                  child: const TabBar(
-                    labelColor: Color(0xff4fbe9e),
-                    unselectedLabelColor: Colors.grey,
-                    indicatorWeight: 0.01,
-                    indicatorColor: Colors.transparent,
-                    tabs: [
-                      Tab(text: 'Upcoming'),
-                      Tab(text: 'Finished'),
-                      Tab(text: 'Favorites'),
-                    ],
-                  ),
-                ),
-              ),
-              const Padding(
-                padding: EdgeInsets.symmetric(vertical: 16),
-                child: SizedBox(
-                  height: 635,
-                  child: TabBarView(
-                    children: [
-                      Upcoming(),
-                      Finished(),
-                      Favorites(),
-                    ],
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.1),
+                          spreadRadius: 1,
+                          blurRadius: 4,
+                          offset: const Offset(0, 4),
+                        ),
+                      ],
+                      color: Colors.white,
+                    ),
+                    child: const TabBar(
+                      labelColor: Color(0xff4fbe9e),
+                      unselectedLabelColor: Colors.grey,
+                      indicatorWeight: 0.01,
+                      indicatorColor: Colors.transparent,
+                      tabs: [
+                        Tab(text: 'Upcoming'),
+                        Tab(text: 'Finished'),
+                        Tab(text: 'Favorites'),
+                      ],
+                    ),
                   ),
                 ),
-              ),
-            ],
+                const Padding(
+                  padding: EdgeInsets.only(top: 8),
+                  child: SizedBox(
+                    height: 600,
+                    child: TabBarView(
+                      children: [
+                        Upcoming(),
+                        Finished(),
+                        Favorites(),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
--- lib/features/trips/presentation/widgets/favorites.dart
+++ lib/features/trips/presentation/widgets/favorites.dart
@@ -1,147 +1,188 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'package:hotel_booking_algoriza/features/trips/presentation/cubit/trips_cubit.dart';
 
 class Favorites extends StatelessWidget {
   const Favorites({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: 3,
-      shrinkWrap: true,
-      itemBuilder: (context, index) => Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-        child: Column(
-          children: [
-            Container(
-              width: 350,
-              height: 150,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: const BorderRadius.all(Radius.circular(16.0)),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey.withOpacity(0.1),
-                    spreadRadius: 1,
-                    blurRadius: 4,
-                    offset: const Offset(0, 4),
-                  ),
-                ],
-              ),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Container(
-                    width: 130,
-                    height: double.infinity,
-                    decoration: const BoxDecoration(
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(16.0),
-                        bottomLeft: Radius.circular(16.0),
-                      ),
-                      image: DecorationImage(
-                        image: NetworkImage(
-                            'https://images.pexels.com/photos/261102/pexels-photo-261102.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'),
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                  ),
-                  Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.all(8),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          RichText(
-                            text: const TextSpan(
-                              style: TextStyle(color: Colors.black),
-                              children: [
-                                TextSpan(
-                                  text: 'Grand Royal Hotel \n',
-                                  style: TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 16,
-                                  ),
-                                ),
-                                TextSpan(
-                                  text: 'Wembley, London',
-                                  style: TextStyle(
-                                    color: Colors.grey,
-                                  ),
-                                ),
-                              ],
+    return BlocConsumer<TripsCubit, TripsState>(
+      listener: (context, state) {},
+      builder: (context, state) {
+        return state is TripsLoadingState
+            ? const Center(
+                child: CircularProgressIndicator(),
+              )
+            : ListView.builder(
+                itemCount: BlocProvider.of<TripsCubit>(context)
+                    .cancelledHotels!
+                    .hotelModel
+                    .booking
+                    .length,
+                shrinkWrap: true,
+                itemBuilder: (context, index) => Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                  child: Column(
+                    children: [
+                      Container(
+                        width: 350,
+                        height: 150,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius:
+                              const BorderRadius.all(Radius.circular(16.0)),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey.withOpacity(0.1),
+                              spreadRadius: 1,
+                              blurRadius: 4,
+                              offset: const Offset(0, 4),
                             ),
-                          ),
-                          const Spacer(),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Row(
-                                children: const [
-                                  Icon(
-                                    Icons.location_on_rounded,
-                                    size: 16,
-                                    color: Color(0xff4fbe9e),
-                                  ),
-                                  Text(
-                                    '2.0 km to city',
-                                    style: TextStyle(
-                                      fontSize: 14,
-                                      color: Colors.grey,
+                          ],
+                        ),
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Container(
+                              width: 130,
+                              height: double.infinity,
+                              decoration: BoxDecoration(
+                                borderRadius: const BorderRadius.only(
+                                  topLeft: Radius.circular(16.0),
+                                  bottomLeft: Radius.circular(16.0),
+                                ),
+                                image: DecorationImage(
+                                  image: BlocProvider.of<TripsCubit>(context)
+                                          .cancelledHotels!
+                                          .hotelModel
+                                          .booking[index]
+                                          .hotel
+                                          .hotelImages
+                                          .isNotEmpty
+                                      ? NetworkImage(
+                                          'http://api.mahmoudtaha.com/images/${BlocProvider.of<TripsCubit>(context).cancelledHotels!.hotelModel.booking[index].hotel.hotelImages[0].image}')
+                                      : const NetworkImage(
+                                          'https://hiueduonline.com/wp-content/plugins/tutor/assets/images/placeholder.jpg'),
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
+                            Expanded(
+                              child: Padding(
+                                padding: const EdgeInsets.all(8),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    RichText(
+                                      text: TextSpan(
+                                        style: const TextStyle(
+                                            color: Colors.black),
+                                        children: [
+                                          TextSpan(
+                                            text:
+                                                '${BlocProvider.of<TripsCubit>(context).cancelledHotels!.hotelModel.booking[index].hotel.name} \n',
+                                            style: const TextStyle(
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: 16,
+                                              color: Colors.black,
+                                            ),
+                                          ),
+                                          TextSpan(
+                                            text:
+                                                '${BlocProvider.of<TripsCubit>(context).cancelledHotels!.hotelModel.booking[index].hotel.address} \n',
+                                            style: const TextStyle(
+                                              color: Colors.grey,
+                                            ),
+                                          ),
+                                        ],
+                                      ),
                                     ),
-                                  ),
-                                ],
-                              ),
-                              const Text(
-                                '\$180',
-                                style: TextStyle(
-                                  color: Colors.black,
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.bold,
+                                    const Spacer(),
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Row(
+                                          children: const [
+                                            Icon(
+                                              Icons.location_on_rounded,
+                                              size: 16,
+                                              color: Color(0xff4fbe9e),
+                                            ),
+                                            Text(
+                                              '2.0 km to city',
+                                              style: TextStyle(
+                                                fontSize: 14,
+                                                color: Colors.grey,
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                        Text(
+                                          '\$ ${BlocProvider.of<TripsCubit>(context).cancelledHotels!.hotelModel.booking[index].hotel.price}',
+                                          style: const TextStyle(
+                                            color: Colors.black,
+                                            fontSize: 16,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                    const SizedBox(height: 8),
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        RatingBar.builder(
+                                          minRating: 1,
+                                          maxRating: 5,
+                                          initialRating: double.parse(
+                                              BlocProvider.of<TripsCubit>(
+                                                      context)
+                                                  .cancelledHotels!
+                                                  .hotelModel
+                                                  .booking[index]
+                                                  .hotel
+                                                  .rate),
+                                          allowHalfRating: true,
+                                          direction: Axis.horizontal,
+                                          itemCount: 5,
+                                          itemSize: 16,
+                                          itemPadding:
+                                              const EdgeInsets.symmetric(
+                                            horizontal: 2.0,
+                                          ),
+                                          itemBuilder: (context, _) =>
+                                              const Icon(
+                                            Icons.star,
+                                            color: Color(0xff4fbe9e),
+                                          ),
+                                          onRatingUpdate: (double value) {},
+                                        ),
+                                        const Text(
+                                          '/per night',
+                                          style: TextStyle(
+                                            fontSize: 14,
+                                            color: Colors.grey,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ],
                                 ),
                               ),
-                            ],
-                          ),
-                          const SizedBox(height: 8),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              RatingBar.builder(
-                                minRating: 1,
-                                maxRating: 5,
-                                initialRating: 4.5,
-                                allowHalfRating: true,
-                                direction: Axis.horizontal,
-                                itemCount: 5,
-                                itemSize: 16,
-                                itemPadding: const EdgeInsets.symmetric(
-                                  horizontal: 2.0,
-                                ),
-                                itemBuilder: (context, _) => const Icon(
-                                  Icons.star,
-                                  color: Color(0xff4fbe9e),
-                                ),
-                                onRatingUpdate: (double value) {},
-                              ),
-                              const Text(
-                                '/per night',
-                                style: TextStyle(
-                                  fontSize: 14,
-                                  color: Colors.grey,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ],
+                            )
+                          ],
+                        ),
                       ),
-                    ),
-                  )
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
+                    ],
+                  ),
+                ),
+              );
+      },
     );
   }
 }
